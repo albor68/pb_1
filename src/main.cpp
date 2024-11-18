c61b5457c816d27d77c58e72f4398b7c3fdb01a2
Alexander Borisov <albor68@gmail.com>
2024-11-18 11:16:12 +0300

    Проект завершен успешно

--- src/main.cpp
+++ src/main.cpp
@@ -9,58 +9,56 @@
 void KarelProgram() {
     Move();
     Move();
-    Move();
     TurnLeft();
     TurnLeft();
     TurnLeft();
     Move();
-    Move();
-    Move();
     TurnLeft();
-    PickBeeper();
-
-    TurnLeft();
-    TurnLeft();
-    TurnLeft();
-    Move();
-    Move();
-    Move();
-    TurnLeft();
-    Move();
-    Move();
     Move();
     PickBeeper();
-
-    Move();
-    TurnLeft();
-    TurnLeft();
-    TurnLeft();
-    Move();
-    TurnLeft();
-
-    Move();
-    TurnLeft();
-    TurnLeft();
-    TurnLeft();
-    Move();
-    TurnLeft();
-
-    Move();
-    TurnLeft();
-    TurnLeft();
-    TurnLeft();
-    Move();
-    TurnLeft();
-
-    Move();
-    TurnLeft();
-    TurnLeft();
-    TurnLeft();
-    Move();
-    TurnLeft();
- 
 }
-
+//    TurnLeft();
+//    TurnLeft();
+//    TurnLeft();
+//    Move();
+//    Move();
+//    Move();
+//    TurnLeft();
+//    Move();
+//    Move();
+//    Move();
+//    PickBeeper();
+//
+//    Move();
+//    TurnLeft();
+//    TurnLeft();
+//    TurnLeft();
+//    Move();
+//    TurnLeft();
+//
+//    Move();
+//    TurnLeft();
+//    TurnLeft();
+//    TurnLeft();
+//    Move();
+//    TurnLeft();
+//
+//    Move();
+//    TurnLeft();
+//    TurnLeft();
+//    TurnLeft();
+//    Move();
+//    TurnLeft();
+//
+//    Move();
+//    TurnLeft();
+//    TurnLeft();
+//    TurnLeft();
+//    Move();
+//    TurnLeft();
+// 
+//}
+//
 
 int main() {
     LoadWorld("worlds/collect_newspaper.w");
